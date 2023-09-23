9e6699e80a5f08d5407f84fa7ff5e303547711de
VizB <122339161+VizB@users.noreply.github.com>
2023-09-22 17:47:43 -0700

    Adding placeholder stuff as a starting point.

---
+++ main.js
@@ -0,0 +1,9 @@
+// This is a temporary js file until we move our code over to Svelte
+// Just adding a simple log command to make sure it works.
+// Trust svelte is wayyyy better. But the only issue is that we might not have time to learn it
+
+let counter = 0;
+function printHelloWorld() {
+    console.log("Hello World " + counter);
+    counter++;
+}
