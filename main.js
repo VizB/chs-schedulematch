3148dd1df1f78e1142119512b788d16a9b2f57a2
VizB <122339161+VizB@users.noreply.github.com>
2023-09-23 12:21:04 -0700

    Added Form functionality and attached js script to html

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-let counter = 0;
+window.onload = function(){
+    let loginForm = document.getElementsByClassName("loginForm");
+    loginForm[0].addEventListener("submit", (e) => {
+        e.preventDefault();
 
-function printHelloWorld() {
-    console.log("Hello World " + counter);
-    counter++;
+        let name = document.getElementById("name");
+        let year = document.getElementById("year");
+
+        console.log(name.value);
+        console.log(year.value);
+        if(name.value === "" || year.value === "") {
+            throw Error
+        } else {
+            // Do stuff
+        }
+    });
 }
 
-// Stuff for the form
-let loginForm = document.getElementsByClassName("loginForm");
-
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let name = document.getElementById("name");
-    let year = document.getElementById("year");
-
-    if(name.value == "" || year.value == "") {
-        throw Error
-    } else {
-        // Do stuff
+function parseName(name) {
+    for (let i = 0; i < name.length; i++) {
+        if(isNaN(name[i]) === false) {
+            return "Invalid Name"
+        }
     }
-});
+}
